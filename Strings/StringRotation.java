effd8d367259d854edef4ab434d328e35f72a3cc
Chinmai Srikanth Savirigana <97657315+chinmaisavirigana@users.noreply.github.com>
2023-02-24 08:05:41 +0530

    Update StringRotation.java

--- Strings/StringRotation.java
+++ Strings/StringRotation.java
@@ -1,5 +1,49 @@
 class StringRotation {
-    public static boolean rotateString(String s, String goal) {
+    //Best and optimised approach
+
+  public static boolean isRotation(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        
+        String s1s1 = s1 + s1;
+        int n = s2.length();
+        int j = 0;
+        
+        for (int i = 0; i < s1s1.length(); i++) {
+            if (j == n) {
+                return true;
+            }
+            
+            if (s1s1.charAt(i) == s2.charAt(j)) {
+                j++;
+            } else {
+                j = 0;
+            }
+        }
+        
+        return false;
+    }  
+  
+    public static void main(String[] args) {
+        boolean ans = isRotation("hello","elloh");
+        System.out.println(ans);
+    }
+
+
+
+}
+
+/* --> Contains() function
+class Solution {
+    public boolean rotateString(String s, String goal) {
+     return (s.length() == goal.length() && (s+s).contains(goal));   
+    }
+}
+*/
+
+/* --> Time - O(n^2) - because of using substring function
+public static boolean rotateString(String s, String goal) {
         if(s.length()!=goal.length()) return false;
         
         char c = goal.charAt(0);
@@ -15,19 +59,4 @@ class StringRotation {
         }
     return check;
     }
-    public static void main(String[] args) {
-        boolean ans = rotateString("hello","elloh");
-        System.out.println(ans);
-    }
-
-
-
-}
-
-/*
-class Solution {
-    public boolean rotateString(String s, String goal) {
-     return (s.length() == goal.length() && (s+s).contains(goal));   
-    }
-}
 */
