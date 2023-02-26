775388f1728b1cffa0d82a1e46b0ec984029b75b
Chinmai Srikanth Savirigana <97657315+chinmaisavirigana@users.noreply.github.com>
2023-02-27 00:10:55 +0530

    Update SumBeautySubstrings.java
    1. Consider every substring from first to last char in string
    2. For every substring get the maximum and minimum frequency count of letters and add it to the sum variable
    3. A substring to be considered only if its beauty(max - min)  frequency is greater than 0.
    4. Use integer array with size 26 to save the frequency of every alphabet
    5. TC --> O(N^2) & SC--> O(1)

--- Strings/SumBeautySubstrings.java
+++ Strings/SumBeautySubstrings.java
@@ -25,3 +25,39 @@ public class SumBeautySubstrings {
     }
     
 }
+/*
+
+/*
+public int beautySum(String s) {
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++){
+            for(int j = i+1; j <= s.length(); j++){
+                String sub = s.substring(i,j);
+                int beauty = getBeauty(sub);
+                sum+=beauty;
+            }
+        }
+        return sum;
+    }
+
+    public int getBeauty(String sub){
+        int[] alpha = new int[26];
+        for(int i =0;i<sub.length();i++){
+            alpha[sub.charAt(i)-'a']++;
+        }
+
+        int maxi = Integer.MIN_VALUE;
+        int mini = Integer.MAX_VALUE;
+       
+        for(int i = 0;i < 26; i++){
+            if(alpha[i]!=0){
+            maxi = Math.max(alpha[i],maxi);
+            mini = Math.min(alpha[i],mini);
+            }
+        }
+       
+        return maxi-mini;
+
+    }
+*/
+*/
